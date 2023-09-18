609ce1f610e03379fb8d2475fd1a5cc2deabd091
Ethan Hu <ethanhu147@gmail.com>
2023-09-18 13:08:14 -0400

    finished the selling of all 4 ingredients

--- Store.java
+++ Store.java
@@ -9,9 +9,27 @@ public class Store{
         int sugar = 0;
         double money = 100.00;
         Scanner scan = new Scanner(System.in);
-        System.out.print("How many lemons?");
-        lemon = scan.nextInt();
-        money = money - 0.19*lemon;
+        
+        System.out.println("How many lemons?");
+        if(money - 0.29*lemon>0){
+        lemon = lemon + scan.nextInt();
+        money = String.format(money - 0.29*lemon);
         System.out.println("Lemon:"+lemon+" $:"+money);
+        }
+
+        System.out.println("How much ice?");
+        ice = ice + scan.nextInt();
+        money = money - 0.09*ice;
+        System.out.println("Ice:"+ice+" $:"+money);
+
+        System.out.println("How many cups?");
+        cup = cup + scan.nextInt();
+        money = money - 0.49*cup;
+        System.out.println("Cups:"+cup+" $:"+money);
+
+        System.out.println("How much sugar?");
+        sugar = sugar + scan.nextInt();
+        money = money - 0.19*sugar;
+        System.out.println("Sugar:"+sugar+" $:"+money);
     }
 }
