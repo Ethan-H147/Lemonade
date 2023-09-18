45be04477dbfe2f9acde10732ba72f6805b47a0c
Ethan Hu <ethanhu147@gmail.com>
2023-09-18 12:41:20 -0400

    Created two files and started making the buy phase , which you buy ingredients and decide your recipes

---
+++ Sell.java
@@ -0,0 +1,7 @@
+public class Sell {
+    public static void main(String[] args)
+    {
+        New buyphase = buyphase;
+
+    }
+}
---
+++ Store.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Store{
+    public void buyphase()
+    {
+        int lemon;
+        int ice;
+        int cup;
+        int sugar;
+        double money = 100.00;
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Buy lemon?");
+        
+    }
+}
